730b94f623afd1de68fa4f9b13fcc148546cfbb5
Katia <Katia@DESKTOP-N5GL2A8>
2017-02-23 00:06:43 +0200

    k

--- harjoitus/harjoitus/huone1.xaml.cs
+++ harjoitus/harjoitus/huone1.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using harjoitusClass;
 
 namespace harjoitus
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for huone1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class huone1 : Window
     {
-        Esine table = new Esine { Numero = 1, Image = "pictures/1.png" };
-        public MainWindow()
+        public huone1()
         {
             InitializeComponent();
-        }
-
-        private void OnImageButtonClick(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
