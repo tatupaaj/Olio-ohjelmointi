f02ed1d83dc360e6ce410c03244cfac4e919c19c
K8760 <K8760@ICT-421-WS-27>
2017-03-20 13:50:07 +0200

    k

---
+++ Labra14/WpfVRTrains/APTrain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class APTrain
+    {
+        public static string GetJsonFROMLiikeVirasto(string filter)
+        {
+            try
+            {
+                string url = @"http://rata.digitraffic.fi/api/v1/live-trains?station=" + filter;
+                using (WebClient wc = new WebClient())
+                {
+                    string json = wc.DownloadString(url);
+                    return json;
+
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Labra14/WpfVRTrains/VMTrains.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JAMK.IT
+{
+    public class TrainsVM
+    {
+        public static List<JAMK.IT.Train> GetTrainsAt(string station)
+        {
+            List<Train> trains = new List<Train>();
+            if (station == "testi" || station == "")
+            {
+                //Vaihde1: Placebo palauttaa oikea muotoista dataa 
+                // keksitään muutama juna
+                Train tr = new Train();
+                tr.TrainNumber = "666";
+                tr.DepDate = new DateTime(2017, 3, 21);
+                tr.TargetStation = "Higway to Hell";
+                trains.Add(tr);
+                
+            }
+            else
+            {
+                //Vaihde2: haetaan Liikenneviraston API:sta
+                //muutetaan haettu json olio-kokoelmaksi
+                string tmp = JAMK.IT.APTrain.GetJsonFROMLiikeVirasto(station);
+                trains = JsonConvert.DeserializeObject<List<Train>>(tmp);
+            }
+            //palautus
+            return trains;
+        }
+    }
+}
